c3f2a8b9a6bcee178337ea19370b5c468f75ea9a
hyebin <sksp0401@gmail.com>
2024-07-10 13:39:08 +0900

    Fix: kakao region 타입 수정

--- types/region.ts
+++ types/region.ts
@@ -1,5 +1,19 @@
 export interface KakaoLocalWithGeoType {
   documents: {
+    address: {
+      address_name: string;
+      b_code: string;
+      h_code: string;
+      main_address_no: string;
+      mountain_yn: string;
+      region_1depth_name: string;
+      region_2depth_name: string;
+      region_3depth_h_name: string;
+      region_3depth_name: string;
+      sub_address_no: string;
+      x: string;
+      y: string;
+    };
     region_type: string;
     code: string;
     address_name: string;
