83b12e7e67819596a8c074f03b6b8612d50bac76
hyebin <sksp0401@gmail.com>
2024-07-10 13:41:44 +0900

    Fix: 사용하지 않는 dong region 타입 삭제

--- types/region.ts
+++ types/region.ts
@@ -29,37 +29,6 @@ export interface KakaoLocalWithGeoType {
   };
 }
 
-export interface DongRegionType {
-  response: {
-    service?: {
-      name: "search";
-      version: "2.0";
-      operation: "search";
-      time: string;
-    };
-    status: "OK" | "NOT_FOUND";
-    record?: {
-      total: string;
-      current: string;
-    };
-    page?: {
-      total: string;
-      current: string;
-      size: string;
-    };
-    result?: {
-      crs: string;
-      type: string;
-      items: {
-        id: string;
-        title: string;
-        geometry: string;
-        point: { x: string; y: string };
-      }[];
-    };
-  };
-}
-
 export interface UserRegionReqType {
   x?: string;
   y?: string;
